95387126103d95c92b20fef67693fb7e1c25ed3a
IdoShapira <ishapira@yotpo.com>
2018-05-20 14:52:47 +0300

    main-menu

--- app/src/main/java/com/afeka/finalproject/collectionpoints/MainMenuActivity.java
+++ app/src/main/java/com/afeka/finalproject/collectionpoints/MainMenuActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainMenuActivity extends AppCompatActivity {
 
-    private String userId;
+    private User user;
     private FirebaseAuth mAuth;
 
 
@@ -22,6 +25,18 @@ public class MainMenuActivity extends AppCompatActivity {
 
     void pullUserData(){
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
        // StringmAuth.getCurrentUser()
     }
 }
