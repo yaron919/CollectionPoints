5311031e924fc3c52bf532689181127a06cf029c
Yaron Ashual <yaron919@gmail.com>
2018-05-20 13:42:53 +0300

    add all activity

---
+++ app/src/main/java/com/afeka/finalproject/collectionpoints/PointsInsert.java
@@ -0,0 +1,13 @@
+package com.afeka.finalproject.collectionpoints;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PointsInsert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_points_insert);
+    }
+}
